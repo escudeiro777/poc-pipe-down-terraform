70933574bcf7caf05b6f3ef26480694f96534310
nayara-nascimento <nayara.nascimento@darede.com.br>
2024-08-20 11:12:43 -0300

    fix: fix the providers file

--- poc-pipe-down/scripts/providers.tf
+++ poc-pipe-down/scripts/providers.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   region                   = var.aws_region
-  shared_config_files      = ["~/.aws/config"]
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "consulting"
+  # shared_config_files      = ["~/.aws/config"]
+  # shared_credentials_files = ["~/.aws/credentials"]
+  # profile                  = "consulting"
 }
