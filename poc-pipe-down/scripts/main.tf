fb2347dc1166afd0bfe5613a6f768cd7a22799d8
nayara-nascimento <nayara.nascimento@darede.com.br>
2024-08-20 10:59:54 -0300

    fix: fix the tf code

--- poc-pipe-down/scripts/main.tf
+++ poc-pipe-down/scripts/main.tf
@@ -1,3 +1,7 @@
+data "aws_db_instance" "db_instance" {
+  db_instance_identifier = "dev-rds-instance"
+}
+
 resource "aws_db_snapshot" "db_snapshot" {
   depends_on = [data.aws_db_instance.db_instance]
 
@@ -5,14 +9,10 @@ resource "aws_db_snapshot" "db_snapshot" {
   db_snapshot_identifier = "dev-rds-instance-snapshot-dois"
 }
 
-data "aws_db_instance" "db_instance"{
-    db_instance_identifier = "dev-rds-instance"
-}
-
 resource "null_resource" "execute_delete_script" {
   provisioner "local-exec" {
     command = "python3 delete-rds.py"
   }
 
-  depends_on = [aws_db_snapshot.rds_snapshot]
+  depends_on = [aws_db_snapshot.db_snapshot]
 }
